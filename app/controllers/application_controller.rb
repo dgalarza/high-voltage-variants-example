540ac11e839de28281b0172d32f0b2921c4faa8d
Damian Galarza <damian@thoughtbot.com>
2014-11-13 13:45:40 -0500

    Set up HighVoltage variants example

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_variant
+
+  private
+
+  def set_variant
+    if browser.mobile?
+      request.variant = :mobile
+    elsif browser.tablet?
+      request.variant = :tablet
+    end
+  end
 end
